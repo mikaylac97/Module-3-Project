c610e16005f84223fbd97824fc57cd402658eff8
Mikayla Castro <mikaylacastro97@gmail.com>
2020-11-07 16:57:40 -0500

    route change to get user reviews

--- routes/review.routes.js
+++ routes/review.routes.js
@@ -3,9 +3,23 @@ const router = express.Router();
 const passport = require('passport');
 
 const Review = require('../models/Review.model.js');
-const Book = require('../models/Book.model.js')
+const Book = require('../models/Book.model.js');
+const User = require('../models/User.model');
 
 
+
+// GET User's reviews
+
+router.get('/api/reviews/:userId', (req, res, next) => {
+    User.findById(req.params.userId)
+    .populate('reviews')
+        .then(foundUser => {
+            res.json({
+                userReviews: foundUser.reviews
+            })
+        })
+        .catch(err => console.log(`Error finding user's reviews: ${err}`))
+})
 
 
 // POST review to book
@@ -17,11 +31,15 @@ router.post('/api/review/:bookId', (req, res, next) => {
         content: req.body.content
     })
     .then(newReviewFromDB => {
-        Book.findByIdAndUpdate(req.params.bookId, {$push: {reviews: newReviewFromDB._id}})
-            .then(updatedBook => res.json({
-                bookWithNewReview: updatedBook
-            }))
-            .catch(err => console.log(`Error updating book with new commment in DB: ${err}`))
+        User.findByIdAndUpdate(req.session.passport.user, {$push: {reviews: newReviewFromDB._id}})
+            .then(reviewWithUpdatedUser => {
+                Book.findByIdAndUpdate(req.params.bookId, {$push: {reviews: reviewWithUpdatedUser}})
+                .then(updatedBook => res.json({
+                    bookWithNewReview: updatedBook
+                }))
+                .catch(err => console.log(`Error updating book with new commment in DB: ${err}`))
+            })
+            .catch(err => console.log(`Error updating user with review: ${err}`))
     })
     .catch(err => console.log(`Error finding book in DB: ${err}`))
 })
